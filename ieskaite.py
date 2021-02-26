4e8c563142310d3731f00abe833dcf90f489dd9c
Eva Martinsone <mmmartinsone@gmail.com>
2021-02-26 18:59:21 +0200

    cik varju tik dariju

---
+++ ieskaite.py
@@ -0,0 +1,41 @@
+## 1. dzejolis
+print('Mācies, kamēr gudrs tiec,Visu labi vērā liec.Dzīve gudra, viltīga -\n\t Gudrība tur derīga!')
+
+#2. python versija
+## ievada terminali python --version
+Python 3.9.1
+
+## 3. Riņķa radiuss
+a = 2
+b = 3,14
+print(a * b)
+
+## 4. Vārda seciba
+a = input('ievadi vardu')
+b = a[::-1]
+print(b)
+
+## 5. 10 skaitļu summa
+a = [1, 2, 3, 4, 5]
+for i in range(len(a)):
+    print(a[i])
+
+## 6. Preces saraksts
+preces = ['pirmā', 'otrā', 'trešā', 'ceturtā', 'piektā']
+vajadzigas = res = [preces[0], preces[-1]]
+print(vajadzigas)
+
+## 7.Lietotājs ievada veselu skaitli x aprēķini formulas x2+ (x / 2) *2
+a = int(input())
+print(a ** 2 + (a / 2) * 2)
+
+## 8. Lietotājs ievada veselu skaitli, noskaidrot vai tas atrodas intervālā no 100 līdz 1000
+
+## 9. Izveido programmu, kas aprēķina Ķermeņa Masas Indeksu.
+augums = int(input())
+svars = int(input())
+index = ((svars / augumu) **2)
+
+## 10. 
+# print('Mācies, kamēr gudrs tiec,Visu labi vērā liec.Dzīve gudra, viltīga -\n\t Gudrība tur derīga!')
+print('Man viss izdodas!')
